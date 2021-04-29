84ef0513e664410f8ebe7531d5ff6884009c598e
Jeanmarie Quijada <jeanmarie.q@gmail.com>
2021-04-28 23:36:40 -0300

    install sequelize

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
-
+const { Sequelize, DataTypes } = require('sequelize')
 app.set('view engine', 'ejs')
+
+const sequelize = new Sequelize ({ dialect: 'sqlite', storage: 'cita-database.db' })
+
+app.use(express.json())
 
 app.listen(8080, () => {
     console.log('Iniciando o servidor')
