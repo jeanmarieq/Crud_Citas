2ff1e57c374fa0e34c85871a914ad1f140b53a1e
Jeanmarie Quijada <jeanmarie.q@gmail.com>
2021-04-29 00:23:03 -0300

    add get-post-put-delete

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
 const express = require('express')
 const app = express()
 const { Sequelize, DataTypes } = require('sequelize')
+const CitaModel = require('./models/cita')
+
 app.set('view engine', 'ejs')
 
 const sequelize = new Sequelize ({ dialect: 'sqlite', storage: 'cita-database.db' })
 
+const Citas = CitaModel(sequelize, DataTypes)
+
 app.use(express.json())
+
+// Mostrar lista de citas
+app.get('/citas', async (req, res) => {
+    const allCitas = await Citas.findAll();
+    res.json({ Citas: allCitas})
+})
+
+// Mostrar Cita por ID
+app.get('/citas/:id', async (req, res) => {
+    const citaId = req.params.id
+    const cita = await Citas.findByPk(citaId)
+
+    res.json({ action: 'Mostrar Cita', Citas: cita})
+})
+
+// Criar Cita
+app.post('/citas', async (req, res) => {
+    const novacita = await Citas.create(req.body);
+    /* ({
+        name: req.body.name,
+        date: req.body.date,
+        time: req.body.time,
+        description: req.body.description
+    }) */
+   res.json({ novacita });
+})
+
+// Atualizar cita
+app.put('/citas/:id', async (req, res) => {
+    const citaId = req.params.id
+    const lisCitas = await Citas.findByPk(citaId)
+    lisCitas.update(req.body);
+    /* ({
+        name: req.body.name,
+        date: req.body.date,
+        time: req.body.time,
+        description: req.body.description
+    }) */
+    res.send({ lisCitas: lisCitas })
+})
+  
+// Apagar Cita
+app.delete('/citas/:id', async (req, res) => {
+    const citaId = req.params.id
+    const apagarCita = await Citas.destroy({ where: {id: citaId} })
+  
+    res.send({ action: 'Apagar cita', apagarCita: apagarCita})
+})
 
 app.listen(8080, () => {
     console.log('Iniciando o servidor')
---
+++ seeders/20210429031019-demo-cita.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert('Cita', [{
+        name: 'John Doe',
+        date: '10-06-2021',
+        time: '08:00',
+        description: 'febre',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        name: 'Maria Silva',
+        date: '05-08-2021',
+        time: '10:00',
+        description: 'Dor',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        name: 'Tiago Perez',
+        date: '20-10-2021',
+        time: '01:00',
+        description: 'alergia',
+        createdAt: new Date(),
+        updatedAt: new Date()
+    }], {});
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
